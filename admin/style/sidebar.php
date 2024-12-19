df1ead2771fa546eee5699292b7dcaa2725e295d
fthndev <alizainfathan@gmail.com>
2024-12-20 01:03:23 +0700

    terbaru sedikit

--- admin/style/sidebar.php
+++ admin/style/sidebar.php
@@ -2,7 +2,7 @@
     <h5 class="text-center my-3">Rempah</h5>
     <ul class="nav flex-column">
         <li class="nav-item">
-            <a class="nav-link" href="../dashboard.php"><i class="bi bi-speedometer2"></i> Dashboard</a>
+            <a class="nav-link" href="dashboard.php"><i class="bi bi-speedometer2"></i> Dashboard</a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="../../artikel.php"><i class="bi bi-people"></i> Artikel</a>
@@ -11,7 +11,7 @@
             <a class="nav-link" href="../../rempah.php"><i class="bi bi-clipboard"></i> Rempah</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="../../olahan.php"><i class="bi bi-house"></i> Olahan</a>
+            <a class="nav-link" href="manage_user.php"><i class="bi bi-house"></i> Olahan</a>
         </li>
     </ul>
 </div>
